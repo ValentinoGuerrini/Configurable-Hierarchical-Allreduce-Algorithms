5e998cb01c9602c89d3a4de07bb527e397d3c10c
Valentino Guerrini <valentino.guerrini01@gmail.com>
2025-11-12 18:24:32 +0000

    add aurora test 64 2 8

--- testing/custom_implementations/work_dir/all_gather/tester_aurora_allgather.sh
+++ testing/custom_implementations/work_dir/all_gather/tester_aurora_allgather.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
-#PBS -l select=64
+#PBS -l select=2
 #PBS -l place=scatter
 #PBS -l walltime=0:30:00
 #PBS -l filesystems=home_fs
-#PBS -q prod
+#PBS -q debug
 #PBS -A UIC-HPC
 cd ${PBS_O_WORKDIR}
 
 # MPI and OpenMP settings
 NNODES=`wc -l < $PBS_NODEFILE`
-NRANKS=32 # Number of MPI ranks to spawn per node
+NRANKS=64 # Number of MPI ranks to spawn per node
 NDEPTH=1 # Number of hardware threads per rank (i.e. spacing between MPI ranks)
 NTHREADS=1 # Number of software threads per rank to launch (i.e. OMP_NUM_THREADS)
 
@@ -19,4 +19,4 @@ echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NR
 
 # Change the directory to work directory, which is the directory you submit the job.
 cd $PBS_O_WORKDIR
-mpiexec --np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth -env OMP_NUM_THREADS=${NTHREADS} ./out 14 --overwrite b=64 base=1 num_nodes=${NNODES} radix_increment=5
+mpiexec --np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth -env OMP_NUM_THREADS=${NTHREADS} ./out 13 --overwrite b=8 base=1 num_nodes=${NNODES} radix_increment=1
